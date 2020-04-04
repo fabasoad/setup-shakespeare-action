877aa093b3c467cb4547517c128d0878593820dc
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-04-04 23:28:01 +0900

    Add setuptools installation

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ const { exec } = require('@actions/exec');
 
 const version = core.getInput('version');
 
+const SETUPTOOLS_VERSION = '46.1.3';
+
 (async () => {
   try {
     const sudo = process.platform !== 'win32' ? 'sudo ' : '';
-    await exec(`${sudo}pip3 install shakespearelang` + (version ? `==${version}` : ''));
+    await exec(`${sudo}pip3 install setuptools==${SETUPTOOLS_VERSION} shakespearelang` + (version ? `==${version}` : ''));
   } catch (e) {
     core.setFailed(e.message);
   }
