e283e69672be0045ca56b15f31a771f84a997725
fabasoad <fabasoad@gmail.com>
2025-02-07 13:10:24 +0900

    Add github-token input

--- src/collect-info.sh
+++ src/collect-info.sh
@@ -9,6 +9,9 @@ LIB_DIR_PATH="${SRC_DIR_PATH}/lib"
 main() {
   input_force="${1}"
 
+  python_installed=$(if command -v python >/dev/null 2>&1; then echo true; else echo false; fi)
+  echo "python-installed=${python_installed}" >> "$GITHUB_OUTPUT"
+
   bin_installed="false"
   if command -v shakespeare >/dev/null 2>&1; then
     if [ "${input_force}" = "false" ]; then
@@ -22,12 +25,6 @@ main() {
     log_info "shakespeare is not found. Executing installation."
   fi
   echo "bin-installed=${bin_installed}" >> "$GITHUB_OUTPUT"
-
-  bin_dir="shakespeare_$(date +%s)"
-  echo "bin-dir=${bin_dir}" >> "$GITHUB_OUTPUT"
-
-  bin_path="$GITHUB_WORKSPACE/${bin_dir}"
-  echo "bin-path=${bin_path}" >> "$GITHUB_OUTPUT"
 }
 
 main "$@"
